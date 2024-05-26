8a73138ce17c535b3f26b7fcb2c219535f1c1c11
Rafael Okamoto <rafa.mg1@hotmail.com>
2024-05-25 23:30:10 -0300

    typeORM and DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.development.env' })],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: process.env.DB_DATABASE,
+    })
+  ],
   controllers: [],
   providers: [],
 })
